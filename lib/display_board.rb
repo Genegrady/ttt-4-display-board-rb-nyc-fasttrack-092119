6ff37511a340d9ee8bd1be7b02679722ed2ee795
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-21 18:52:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts " #{board[0]}| #{board[2]} | #{board[3]}"
+  puts " #{board[0]} | #{board[2]} | #{board[3]}"
   puts "-----------"
   puts " #{board[4]} | #{board[5]} | #{board[6]}  "
   puts "-----------"
